21c104af066de673baf48a95025e7b56ac141630
Austin Lippert <6955342+aternex@users.noreply.github.com>
2018-10-29 18:09:30 -0400

    Add class to handle string replacements

---
+++ CmsWeb/Services/StringTokenizer.cs
@@ -0,0 +1,26 @@
+﻿using CmsData;
+using System.Collections.Generic;
+
+namespace CmsWeb.Services
+{
+    public class StringTokenizer
+    {
+        public string TokenizeString(string input, Dictionary<string, string> tokens)
+        {
+            string output = input;
+
+            foreach (var key in tokens.Keys)
+            {
+                output = output.Replace(key, tokens[key]);
+            }
+
+            return output;
+        }
+
+        public string TokenizeContent(string templateName, Dictionary<string, string> tokens)
+        {
+            var contentString = DbUtil.Db.Content(templateName).Body;
+            return TokenizeString(contentString, tokens);
+        }
+    }
+}
